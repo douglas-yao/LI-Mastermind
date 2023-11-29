431fe4df9162230b21f198b635cff6ab7d64a9ff
Douglas Yao <doug.yao@gmail.com>
2023-11-28 16:20:52 -0800

    add logic to handle random number generation - to be replaced by backend logic

--- client/src/features/Game/components/GameBoard.tsx
+++ client/src/features/Game/components/GameBoard.tsx
@@ -1,17 +1,37 @@
 import UserGuessRow from './UserGuesses/UserGuessRow';
 import GameCluesRow from './GameClues/GameCluesRow';
+import { useState, useEffect } from 'react';
 
 export default function GameBoard() {
+  const [solution, setSolution] = useState<number[] | null>(null);
+
+  useEffect(() => {
+    generateRandomArray();
+  }, []);
+
+  function generateRandomArray() {
+    const randomArray = Array.from({ length: 4 }, () =>
+      Math.floor(Math.random() * 8)
+    );
+    setSolution(randomArray);
+  }
+
+  function renderBoardRows() {
+    return rows.map((_, i) => (
+      <div className="flex gap-7">
+        <UserGuessRow key={i} />
+        <GameCluesRow />
+      </div>
+    ));
+  }
+
   const rows = Array.from({ length: 10 }, (_, i) => ({ id: i }));
 
   return (
     <div className="flex flex-col items-center gap-4">
-      {rows.map((_, i) => (
-        <div className="flex gap-7">
-          <UserGuessRow key={i} />
-          <GameCluesRow />
-        </div>
-      ))}
+      <button onClick={generateRandomArray}>Restart</button>
+      <span>{solution}</span>
+      {renderBoardRows()}
     </div>
   );
 }
