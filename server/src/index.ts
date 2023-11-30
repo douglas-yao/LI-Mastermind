62cf67c44f64e256136551a33669d74ea3d20b8c
Douglas Yao <doug.yao@gmail.com>
2023-11-29 20:28:34 -0800

    create initial connection to mysql db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,31 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import mysql from 'mysql';
 import gameRoutes from './routes/gameRoutes';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'mastermind',
+});
+
+app.get('/users', (req: Request, res: Response) => {
+  const q = 'SELECT * FROM users';
+  db.query(q, (err, data) => {
+    console.log('hit users endpoint');
+    if (err) {
+      console.log('error!');
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
 
 app.use('/game', gameRoutes);
 
