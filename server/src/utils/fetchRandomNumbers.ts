608069cd2f937c0ccc39f43198789bb5047934bb
Douglas Yao <doug.yao@gmail.com>
2023-11-30 15:00:39 -0800

    clean up console logs

--- server/src/utils/fetchRandomNumbers.ts
+++ server/src/utils/fetchRandomNumbers.ts
@@ -6,6 +6,5 @@ export default async function getRandomNumber(difficultyLevel?: string) {
   const response = await axios.get(
     'https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new'
   );
-  console.log('response from Random API: ', response);
   return response.data;
 }
