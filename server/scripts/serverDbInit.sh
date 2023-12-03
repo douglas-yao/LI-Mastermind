a253e635e6d6ce192bc48f5fa51565f9157cc459
Douglas Yao <doug.yao@gmail.com>
2023-12-03 13:16:07 -0800

    fix file directory typo

--- server/scripts/serverDbInit.sh
+++ server/scripts/serverDbInit.sh
@@ -7,7 +7,7 @@ read -p "Enter your MySQL username: " MYSQL_USER
 read -sp "Enter your MySQL password: " MYSQL_PASSWORD
 
 # Set the absolute path to the SQL script
-SQL_SCRIPT_PATH="$(pwd)/server/src/schemas/mastermindSchema.sql"
+SQL_SCRIPT_PATH="$(pwd)/src/schemas/mastermindSchema.sql"
 
 # Run the setup SQL script
 if mysql -u $MYSQL_USER -p$MYSQL_PASSWORD < "$SQL_SCRIPT_PATH"; then
