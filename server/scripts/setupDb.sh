1582db153655b8e3c95133f460eeedf8e8b1df55
Douglas Yao <doug.yao@gmail.com>
2023-12-03 12:57:11 -0800

    relocate script files and db schema

--- server/scripts/setupDb.sh
+++ server/scripts/setupDb.sh
@@ -6,11 +6,11 @@ read -p "Enter your MySQL username: " MYSQL_USER
 # Prompt the user for the MySQL password
 read -sp "Enter your MySQL password: " MYSQL_PASSWORD
 
-# Set the path to the SQL script
-SQL_SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")/setupDb.sql"
+# Set the absolute path to the SQL script
+SQL_SCRIPT_PATH="$(pwd)/src/schemas/mastermindSchema.sql"
 
 # Run the setup SQL script
-if mysql -u$MYSQL_USER -p$MYSQL_PASSWORD < $SQL_SCRIPT_PATH; then
+if mysql -u $MYSQL_USER -p$MYSQL_PASSWORD < "$SQL_SCRIPT_PATH"; then
   echo "Database setup successful!"
 else
   echo "Database setup failed. Please check your MySQL credentials and try again."
