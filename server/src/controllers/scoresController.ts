176d99ba688e9e2fc958e8c9207ca682ac86fcdd
Douglas Yao <doug.yao@gmail.com>
2023-12-04 15:51:37 -0800

    wrap logic into try catch blocks

--- server/src/controllers/scoresController.ts
+++ server/src/controllers/scoresController.ts
@@ -1,16 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import UserGameModel from '../models/userGameModel';
-import { Scores, Score } from '../types/types';
 
 const userGameModel = new UserGameModel();
 
 const scoresController = {
   getTopScores: async (req: Request, res: Response, next: NextFunction) => {
-    const { difficulty } = req.body;
-    const topScores = await userGameModel.getTopScores(difficulty, '10');
+    try {
+      const { difficulty } = req.body;
+      const topScores = await userGameModel.getTopScores(difficulty, '10');
 
-    res.locals.scores = topScores;
-    next();
+      res.locals.scores = topScores;
+      next();
+    } catch (error) {
+      console.error('Error fetching top scores:', error);
+      res.status(500).send('Internal Server Error');
+    }
   },
 };
 
