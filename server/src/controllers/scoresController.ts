80d43274133d68091df163f0e495dce8c77dfdcf
Douglas Yao <doug.yao@gmail.com>
2023-12-03 22:35:53 -0800

    clean up console logs

--- server/src/controllers/scoresController.ts
+++ server/src/controllers/scoresController.ts
@@ -5,9 +5,7 @@ import { Scores, Score } from '../types/types';
 const scoresController = {
   getTopScores: async (req: Request, res: Response, next: NextFunction) => {
     const { difficulty } = req.body;
-    console.log('getTopScores req body: ', difficulty);
     const topScores = await userGameModel.getTopScores(difficulty, '10');
-    // console.log(topScores);
 
     res.locals.scores = topScores;
     next();
