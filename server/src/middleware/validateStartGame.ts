88da45f23a317129678e6b74dc5cf67b91f5fe98
Douglas Yao <doug.yao@gmail.com>
2023-12-04 20:25:32 -0800

    add sanitization logic to trim whitespace from userId or set it to Anonymous if empty

--- server/src/middleware/validateStartGame.ts
+++ server/src/middleware/validateStartGame.ts
@@ -1,26 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
 
 /**
- * Middleware to validate the start game request.
+ * Middleware to validate and sanitize the start game request.
  * It checks if userId is a valid string containing only letters and numbers,
  * and if difficulty is a valid string representing difficulty levels (Easy, Normal, Hard).
+ * If userId is an empty string or becomes empty after trimming, it is set to 'Anonymous'.
  *
  * @param req - Express Request object.
  * @param res - Express Response object.
  * @param next - Express NextFunction to pass control to the next middleware.
  */
 const validateStartGame = (req: Request, res: Response, next: NextFunction) => {
-  const { userId, difficulty } = req.body;
+  let { userId, difficulty } = req.body;
 
   // Regex to match only letters and numbers
   const alphanumericRegex = /^[a-zA-Z0-9]+$/;
 
-  // Validate userId
-  if (
-    !userId ||
-    typeof userId !== 'string' ||
-    !alphanumericRegex.test(userId)
-  ) {
+  // Validate and sanitize userId
+  userId = (userId || 'Anonymous').trim() || 'Anonymous';
+
+  if (typeof userId !== 'string' || !alphanumericRegex.test(userId)) {
     return res.status(400).json({
       error:
         'Invalid userId. It should be a string containing only letters and numbers.',
@@ -39,6 +38,8 @@ const validateStartGame = (req: Request, res: Response, next: NextFunction) => {
     });
   }
 
+  // Pass sanitized userId to the next middleware
+  req.body.userId = userId;
   next();
 };
 
