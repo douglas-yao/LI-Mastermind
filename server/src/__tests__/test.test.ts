a12b21ac21fcfded9d95894c016dfbf76eceafd1
Douglas Yao <doug.yao@gmail.com>
2023-12-05 15:43:43 -0800

    create first test

---
+++ server/src/__tests__/test.test.ts
@@ -0,0 +1,9 @@
+describe('games', () => {
+  describe('start game', () => {
+    describe('given validation check fails', () => {
+      it('should return a 404', () => {
+        expect(true).toBe(true);
+      });
+    });
+  });
+});
