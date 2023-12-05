4849c5ff09566b3d88f8dd4e29fdf5a4442ebf40
Douglas Yao <doug.yao@gmail.com>
2023-12-04 19:06:45 -0800

    remove unused modules

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,13 +1,9 @@
-import getRandomSolution from './getRandomSolution';
-import generateFeedback from './generateFeedback';
 import gameLoggingService from './gameLoggingService';
 import gameDbService from './gameDbService';
 import gameManagementService from './gameManagementService';
 import GameCacheService from './gameCacheService';
 
 export {
-  getRandomSolution,
-  generateFeedback,
   gameLoggingService,
   gameDbService,
   gameManagementService,
