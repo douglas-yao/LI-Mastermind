74d6bf52ade615b0fe5a4d2cdd0c081c0d61c491
Douglas Yao <doug.yao@gmail.com>
2023-12-05 19:19:18 -0800

    debug e2e tests for play route, currently still not passing

--- server/src/__tests__/e2e/gameRoute/play.test.ts
+++ server/src/__tests__/e2e/gameRoute/play.test.ts
@@ -14,61 +14,38 @@ const difficultyLevel = 'Normal';
 const solution = '1234';
 const startingGuesses = 10;
 
-beforeEach(() => {
+beforeEach(async () => {
   testGameCacheService = new GameCacheService();
-  testGameCacheService.initializeGameCache(
-    userId,
-    gameId,
-    difficultyLevel,
-    solution,
-    startingGuesses
-  );
+  const test = await request.post('/game/start').send({
+    userId: userId,
+    difficulty: difficultyLevel,
+  });
 });
 
 describe('Game API', () => {
-  describe('POST /game/start', () => {
-    it('should start a new game and return 200 with game data', async () => {
-      const response = await request.post('/game/start').send({
-        userId: 'validUserId',
-        difficulty: 'Normal',
-      });
-
-      expect(response.status).toBe(200);
-      expect(response.body.gameId).toBeTruthy();
-    });
-  });
-
   describe('POST /game/play', () => {
     it('should update the game and return 200 with evaluated game data', async () => {
       const response = await request.post('/game/play').send({
-        userId: 'userId123',
-        guess: '5678',
-        difficulty: 'easy',
+        currentGuess: '4321',
       });
-
+      console.log(response);
       expect(response.status).toBe(200);
-      expect(response.body.feedback.response).toBeTruthy();
+      expect(response.body.guessHistory[0]).toBeTruthy;
+      // expect(response.body.feedback.response).toBeTruthy();
     });
 
     it('should return 400 with invalid guess: contains non-numeric characters', async () => {
       const response = await request.post('/game/play').send({
-        userId: 'userId123',
-        guess: '!@#$', // Invalid guess
-        difficulty: 'easy',
+        guess: '!@#$a',
       });
 
       expect(response.status).toBe(400);
-      expect(response.body).toHaveProperty(
-        'error',
-        'Invalid guess. It should contain only numbers.'
-      );
+      expect(response.body).toHaveProperty('error');
     });
 
     it('should return 400 with invalid guess: incorrect length', async () => {
       const response = await request.post('/game/play').send({
-        userId: 'userId123',
         guess: '123', // Invalid length
-        difficulty: 'easy',
       });
 
       expect(response.status).toBe(400);
