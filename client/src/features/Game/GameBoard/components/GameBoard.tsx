333173b9fd6fc4c76f9cc21d8bf814e747a90135
Douglas Yao <doug.yao@gmail.com>
2023-12-06 10:39:45 -0800

    add a placeholder for the guess submission input field

--- client/src/features/Game/GameBoard/components/GameBoard.tsx
+++ client/src/features/Game/GameBoard/components/GameBoard.tsx
@@ -198,7 +198,7 @@ export default function GameBoard({ difficulty }: GameBoardProps) {
               className="border border-slate-500 py-2 px-4 rounded-lg text-center"
               type="text"
               value={currentGuess}
-              // onChange={(e) => setCurrentGuess(e.target.value)}
+              placeholder="Enter guess here"
               onChange={(e) =>
                 setCurrentGuess(
                   e.target.value.replace(/\D/, '').slice(0, solution.length)
