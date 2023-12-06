06122919c33c5f711f589efb85d29f958b7b5fdf
Douglas Yao <doug.yao@gmail.com>
2023-12-05 17:03:37 -0800

    write out first two successful e2e route tests for /game route

--- server/src/__tests__/test.test.ts
+++ server/src/__tests__/test.test.ts
@@ -1,12 +1,30 @@
 import supertest from 'supertest';
-import { app } from '../index';
+import createServer from '../utils/createServer';
+import pool from '../config/dbConnect';
+
+const app = createServer();
+
+beforeAll(async () => {
+  
+})
 
 describe('game api', () => {
   describe('start endpoint', () => {
     describe('given invalid userId or no difficulty selected', () => {
-      it('should return a 400', async () => {
+      it('should return a 404', async () => {
         const requestBody = { userId: '!@#', difficulty: '' };
         await supertest(app).post('/game/start').send(requestBody).expect(400);
+      });
+    });
+  });
+});
+
+describe('game api', () => {
+  describe('start endpoint', () => {
+    describe('given vaid userId and difficulty selected', () => {
+      it('should return a 404', async () => {
+        const requestBody = { userId: 'testUser', difficulty: 'Normal' };
+        await supertest(app).post('/game/start').send(requestBody).expect(200);
       });
     });
   });
