a938969273f49b723df47824c064c59dde6f6513
Douglas Yao <doug.yao@gmail.com>
2023-12-05 20:47:43 -0800

    disable tests until dbs are mocked

--- server/src/__tests__/e2e/gameRoute/start.test.ts
+++ server/src/__tests__/e2e/gameRoute/start.test.ts
@@ -1,55 +1,59 @@
-import supertest from 'supertest';
-import createServer from '../../../utils/createServer';
+/**
+ * Currently, this test runs, but pushes data straight to the actual database.
+ * Mock the db instead
+ */
 
-const app = createServer();
+// import supertest from 'supertest';
+// import createServer from '../../../utils/createServer';
 
-// Mock the DB instead - currently pushing actual data to the db!
-describe('Game API', () => {
-  describe('POST /start', () => {
-    it('should start a new game and return 200 with game data', async () => {
-      const response = await supertest(app)
-        .post('/game/start')
-        .send({ userId: 'validUserId', difficulty: 'Normal' });
+// const app = createServer();
 
-      expect(response.status).toBe(200);
-      expect(response.body.gameId).toBeTruthy();
-    });
+// describe('Game API', () => {
+//   describe('POST /start', () => {
+//     it('should start a new game and return 200 with game data', async () => {
+//       const response = await supertest(app)
+//         .post('/game/start')
+//         .send({ userId: 'validUserId', difficulty: 'Normal' });
 
-    it("should return 200 and a randomized solution with length 3 for a difficulty of 'Easy'", async () => {
-      const response = await supertest(app)
-        .post('/game/start')
-        .send({ userId: 'validUserId', difficulty: 'Easy' });
+//       expect(response.status).toBe(200);
+//       expect(response.body.gameId).toBeTruthy();
+//     });
 
-      expect(response.status).toBe(200);
-      expect(response.body.currentSolution).toHaveLength(3);
-    });
+//     it("should return 200 and a randomized solution with length 3 for a difficulty of 'Easy'", async () => {
+//       const response = await supertest(app)
+//         .post('/game/start')
+//         .send({ userId: 'validUserId', difficulty: 'Easy' });
 
-    it("should return 200 and a randomized solution with length 4 for a difficulty of 'Normal'", async () => {
-      const response = await supertest(app)
-        .post('/game/start')
-        .send({ userId: 'validUserId', difficulty: 'Normal' });
+//       expect(response.status).toBe(200);
+//       expect(response.body.currentSolution).toHaveLength(3);
+//     });
 
-      expect(response.status).toBe(200);
-      expect(response.body.currentSolution).toHaveLength(4);
-    });
+//     it("should return 200 and a randomized solution with length 4 for a difficulty of 'Normal'", async () => {
+//       const response = await supertest(app)
+//         .post('/game/start')
+//         .send({ userId: 'validUserId', difficulty: 'Normal' });
 
-    it("should return 200 and a randomized solution with length 4 for a difficulty of 'Hard'", async () => {
-      const response = await supertest(app)
-        .post('/game/start')
-        .send({ userId: 'validUserId', difficulty: 'Hard' });
+//       expect(response.status).toBe(200);
+//       expect(response.body.currentSolution).toHaveLength(4);
+//     });
 
-      expect(response.status).toBe(200);
-      expect(response.body.currentSolution).toHaveLength(5);
-    });
-  });
+//     it("should return 200 and a randomized solution with length 4 for a difficulty of 'Hard'", async () => {
+//       const response = await supertest(app)
+//         .post('/game/start')
+//         .send({ userId: 'validUserId', difficulty: 'Hard' });
 
-  it('should return 400 with invalid userId or no difficulty selected', async () => {
-    const response = await supertest(app)
-      .post('/game/start')
-      .send({ userId: `SELECT * FROM games`, difficulty: '' });
+//       expect(response.status).toBe(200);
+//       expect(response.body.currentSolution).toHaveLength(5);
+//     });
+//   });
 
-    expect(response.status).toBe(400);
-  });
+//   it('should return 400 with invalid userId or no difficulty selected', async () => {
+//     const response = await supertest(app)
+//       .post('/game/start')
+//       .send({ userId: `SELECT * FROM games`, difficulty: '' });
 
-  beforeAll(async () => {});
-});
+//     expect(response.status).toBe(400);
+//   });
+
+//   beforeAll(async () => {});
+// });
