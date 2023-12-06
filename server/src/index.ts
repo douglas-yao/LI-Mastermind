b48537bec685d5f4677183e4efb3706cb0fd10e9
Douglas Yao <doug.yao@gmail.com>
2023-12-05 17:03:13 -0800

    wrap creation of app server into a util

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,6 @@
-import gameRoutes from './routes/gameRoutes';
-import scoresRoutes from './routes/scoresRoutes';
 import createServer from './utils/createServer';
 
 const app = createServer();
-
-app.use('/game', gameRoutes);
-app.use('/scores', scoresRoutes);
 
 app.listen(8080, () => {
   console.log('Server listening on port 8080');
--- server/src/utils/createServer.ts
+++ server/src/utils/createServer.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import gameRoutes from '../routes/gameRoutes';
+import scoresRoutes from '../routes/scoresRoutes';
 
 function createServer() {
   const app = express();
@@ -8,6 +10,9 @@ function createServer() {
   app.use(express.json());
   app.use(cors());
   app.use(bodyParser.json());
+
+  app.use('/game', gameRoutes);
+  app.use('/scores', scoresRoutes);
 
   return app;
 }
